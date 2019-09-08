4b6c773a041a239a4eff3213f21e9b19032d63d6
sknubr <54379994+sknubr@users.noreply.github.com>
2019-09-07 20:23:58 -0700

    Update hellomlworld.py

--- hellomlworld.py
+++ hellomlworld.py
@@ -6,7 +6,6 @@ model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 ## Provide the input and output data respectively 
-
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype= float)
 ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
 
@@ -14,7 +13,6 @@ ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
 model.fit(xs,ys,epochs=500)
 
 ## make prediction 
-
 prediction = model.predict([10])
 
 ## print
