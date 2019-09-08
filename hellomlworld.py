3537e36122050523ad959c3e770596f0e9238a1e
sknubr <54379994+sknubr@users.noreply.github.com>
2019-09-07 20:13:00 -0700

    Create hellomlworld.py
    My first example using TensorFlow
    
    credit @lmoroney https://www.youtube.com/watch?v=KNAWp2S3w94

---
+++ hellomlworld.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+## Provide the input and output data respectively 
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype= float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+## fit the data and run 500 times
+model.fit(xs,ys,epochs=500)
+
+## make prediction 
+
+prediction = model.predict([10])
+
+## print
+print (prediction)
